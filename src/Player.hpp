6b15688b9b95218d96eeb3126bbb876cfeec14a8
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-12-31 23:26:34 +0000

    Player class added

---
+++ src/Player.hpp
@@ -0,0 +1,15 @@
+#ifndef __PLAYER__
+#define __PLAYER__
+
+#include "types.hpp"
+
+class Player {
+	friend class Save;
+	private:
+	char m_sCity[20];
+	S_Coordinates m_location = S_Coordinates();
+	int m_iHealth = 3;
+	int m_iMaxHealth = 3;
+};
+
+#endif
