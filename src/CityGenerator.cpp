94e43169ad84c33e0937b4e693a334ce39c44771
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-12-31 15:04:20 +0000

    When horizontal block is finished, find all the city blocks it contains

--- src/CityGenerator.cpp
+++ src/CityGenerator.cpp
@@ -22,6 +22,8 @@ void CityGenerator::_generateGridCity(City& city) {
 	memset(city.grid, WALL_TILE, CITY_SIZE);
 	// make vertical roads
 	_generateVerticalRoads(city);
+	// initialise blocks
+	std::vector<S_CityBlock> blocks;
 	// make horizontal roads
 	_generateHorizontalRoads(city, blocks);
 }
@@ -33,11 +35,11 @@ void CityGenerator::_generateVerticalRoads(City& city) {
 	_divideVerticalBlock(city, b);
 }
 
-void CityGenerator::_generateHorizontalRoads(City& city) {
+void CityGenerator::_generateHorizontalRoads(City& city, std::vector<S_CityBlock> &blocks) {
 	S_Block b;
 	b.index = 0;
 	b.size = CITY_SIZE / CITY_WIDTH;
-	_divideHorizontalBlock(city, b);
+	_divideHorizontalBlock(city, b, blocks);
 }
 
 void CityGenerator::_divideVerticalBlock(City& city, S_Block block) {
@@ -64,8 +66,10 @@ void CityGenerator::_divideVerticalBlock(City& city, S_Block block) {
 	_divideVerticalBlock(city, bRight);
 }
 
-void CityGenerator::_divideHorizontalBlock(City& city, S_Block block) {
+void CityGenerator::_divideHorizontalBlock(City& city, S_Block block, std::vector<S_CityBlock>& blocks) {
 	if (block.size < MIN_DIVIDABLE_HEIGHT) {
+		// for each block of the line, create a city block and add it to blocks
+		_findCityBlocks(city, blocks, block.index * CITY_WIDTH, block.size);
 		return;
 	}
 
@@ -84,6 +88,35 @@ void CityGenerator::_divideHorizontalBlock(City& city, S_Block block) {
 	S_Block bBottom = block;
 	bBottom.index = divideIndex + 2 + widthRoad;
 	bBottom.size = block.index + block.size - divideIndex - 2 - widthRoad;
-	_divideHorizontalBlock(city, bUp);
-	_divideHorizontalBlock(city, bBottom);
+	_divideHorizontalBlock(city, bUp, blocks);
+	_divideHorizontalBlock(city, bBottom, blocks);
+}
+
+void CityGenerator::_findCityBlocks(City& city, std::vector<S_CityBlock>& blocks, int y, int height) {
+	unsigned long nbBlocks = blocks.size();
+	const int STATE_WALL = 0;
+	const int STATE_PAVEMENT_LEFT = 1;
+	const int STATE_ROAD = 2;
+	const int STATE_PAVEMENT_RIGHT = 3;
+	int state = STATE_PAVEMENT_RIGHT;
+	for (int x = 0; x < CITY_WIDTH; ++x) {
+		if (city.grid[x] == WALL_TILE && state == STATE_PAVEMENT_RIGHT) {
+			state = STATE_WALL;
+			blocks.push_back(S_CityBlock());
+			blocks[nbBlocks].x = x;
+			blocks[nbBlocks].y = y;
+			blocks[nbBlocks].height = height;
+		}
+		else if (city.grid[x] == PAVEMENT_TILE && state == STATE_WALL) {
+			state = STATE_PAVEMENT_LEFT;
+			blocks[nbBlocks].width = x - blocks[nbBlocks].x;
+			++nbBlocks;
+		}
+		else if (city.grid[x] == ROAD_TILE && state == STATE_PAVEMENT_LEFT) {
+			state = STATE_ROAD;
+		}
+		else if (city.grid[x] == PAVEMENT_TILE && state == STATE_ROAD) {
+			state = STATE_PAVEMENT_RIGHT;
+		}
+	}
 }
--- src/CityGenerator.hpp
+++ src/CityGenerator.hpp
@@ -9,13 +9,22 @@ typedef struct {
 	int size = 0;
 } S_Block;
 
+typedef struct {
+	int x = 0;
+	int y = 0;
+	int width = 0;
+	int height = 0;
+	int type = 0;
+} S_CityBlock;
+
 class CityGenerator {
 	private:
 	void _generateGridCity(City& city);
 	void _generateVerticalRoads(City& city);
-	void _generateHorizontalRoads(City& city);
+	void _generateHorizontalRoads(City& city, std::vector<S_CityBlock> &blocks);
 	void _divideVerticalBlock(City& city, S_Block block);
-	void _divideHorizontalBlock(City& city, S_Block block);
+	void _divideHorizontalBlock(City& city, S_Block block, std::vector<S_CityBlock> &blocks);
+	void _findCityBlocks(City& city, std::vector<S_CityBlock>& blocks, int y, int height);
 
 	public:
 	void generate(City& city);
