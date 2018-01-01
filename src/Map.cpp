e5b4f4ac3279a49e9365c1221f0367c73d6b2d37
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-01-01 14:17:01 +0000

    Map getters added

---
+++ src/Map.cpp
@@ -0,0 +1,9 @@
+#include "Map.hpp"
+
+int Map::getWidth() {
+	return width;
+}
+
+unsigned int Map::getSize() {
+	return size;
+}
--- src/Map.hpp
+++ src/Map.hpp
@@ -6,6 +6,10 @@ class Map {
 	int width = 0;
 	unsigned int size = 0;
 	char *grid = 0;
+
+	public:
+	int getWidth();
+	unsigned int getSize();
 };
 
 #endif
