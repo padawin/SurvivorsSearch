ce5ebe44d689d1c720f61c830b1ebd7008f75077
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-01-08 13:26:48 +0000

    Initialise the actors with stats

--- src/ActorFactory.hpp
+++ src/ActorFactory.hpp
@@ -4,6 +4,9 @@
 #include "Actor.hpp"
 
 class ActorFactory {
+	private:
+	static void _setAttributes(std::shared_ptr<Actor> actor);
+
 	public:
 	static std::shared_ptr<Actor> createActor(E_ActorRace race, E_ActorType type);
 };
