fa5b8573666d66dc058c79b39f781435e0b3c68c
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-01-17 12:56:31 +0000

    File parser added

---
+++ src/FileParser.cpp
@@ -0,0 +1,32 @@
+#include "FileParser.hpp"
+#include <fstream>
+#include <libgen.h>
+#include <string.h>
+
+const int MAX_CHARS_PER_LINE = 1024;
+
+E_FileParsingResult FileParser::parseFile(const char* file) {
+	std::ifstream fin;
+	fin.open(file);
+	if (!fin.good()) {
+		fin.close();
+		return ERROR_OPENING_FILE;
+	}
+
+	E_FileParsingResult retValue = OK;
+	char *fileDir = dirname(const_cast<char*>(file));
+	strncpy(m_sFileDir, fileDir, MAX_CHAR_DIR_PATH);
+	while (!fin.eof()) {
+		char buf[MAX_CHARS_PER_LINE];
+		fin.getline(buf, MAX_CHARS_PER_LINE);
+
+		if (!_parseLine(buf)) {
+			retValue = ERROR_LINE_PARSING;
+			break;
+		}
+	}
+
+	fin.close();
+
+	return retValue;
+}
