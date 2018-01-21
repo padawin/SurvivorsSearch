4dde8cc616b7a9ba250bb3eb9699c72076d08e32
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-01-21 11:07:06 +0000

    InteractScript class added

---
+++ src/script/Interact.cpp
@@ -0,0 +1,9 @@
+#include "Interact.hpp"
+
+InteractScript::InteractScript(Map &map, int x, int y) :
+	m_map(map),
+	m_iX(x),
+	m_iY(y)
+{
+}
+
---
+++ src/script/Interact.hpp
@@ -0,0 +1,17 @@
+#ifndef __INTERACT_SCRIPT__
+#define __INTERACT_SCRIPT__
+
+#include "Script.hpp"
+#include "../Map.hpp"
+
+class InteractScript : public Script {
+	private:
+	Map &m_map;
+	int m_iX;
+	int m_iY;
+
+	public:
+	InteractScript(Map &map, int x, int y);
+};
+
+#endif
