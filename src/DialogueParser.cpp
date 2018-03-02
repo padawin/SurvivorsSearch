4b6a52d3f111b549d445c00b7f147258bdd51b99
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-03-02 16:40:34 +0000

    Dialogue parser fixed

--- src/DialogueParser.cpp
+++ src/DialogueParser.cpp
@@ -4,14 +4,14 @@
 DialogueParser::DialogueParser(Dialogue &dialogue) : m_dialogue(dialogue) {}
 
 bool DialogueParser::_parseLine(const char *line) {
-	if (m_sCurrentId == "") {
+	if (strncmp(line, "---", 3) == 0) {
+		m_sCurrentId = "";
+	}
+	else if (m_sCurrentId == "") {
 		m_sCurrentId = line;
 	}
-	else if (strncmp(line, "---", 3) == 0) {
-		m_dialogue._addLine(m_sCurrentId, line);
-	}
 	else {
-		m_sCurrentId = "";
+		m_dialogue._addLine(m_sCurrentId, line);
 	}
 	return true;
 }
