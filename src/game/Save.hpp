8a4a9d0f920ece6105559639e3427d66277947ae
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-11-15 22:35:15 +0000

    Save class reorganised

--- src/game/Save.hpp
+++ src/game/Save.hpp
@@ -13,18 +13,16 @@ class Save {
 	private:
 	static void _loadPlayer(std::shared_ptr<GameActor> player, char cityInternalName[20]);
 	static void _loadCity(City &city, char cityName[20]);
-	static void _saveCity(FILE *mapFile, S_CityInfo &city);
+	static void _saveCityLine(FILE *mapFile, S_CityInfo &city);
+	static bool _saveWorld(World &world);
+	static bool _saveCity(City &city);
+	static bool _savePlayer(std::shared_ptr<GameActor> player, City &city);
 
 	public:
-	static void loadWorld(World& world);
-	static void loadCity(std::string internalName, City& city);
 	static bool exists();
 	static void clean();
-	static void create(std::shared_ptr<GameActor> player, City &city);
-	static bool saveWorld(World &world);
-	static bool saveCity(City &city);
-	static bool savePlayer(std::shared_ptr<GameActor> player, City &city);
 
+	static void create(std::shared_ptr<GameActor> player, City &city);
 	static void load(std::shared_ptr<GameActor> player, City &city);
 };
 
